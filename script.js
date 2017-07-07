0186773f032445657e6d92254c0ef57fb03d1856
lucas rodriguez <lucasrodriguez805@gmail.com>
2017-07-07 19:10:05 +0000

    commit message

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // JavaScript File
 // JavaScript File
-$( document ).ready(function() {
 
-
-});
+$(document).ready(function(){
+  $('#apple1').mouseenter(function(){
+    $('#apple1').animate({padding: +'10px'
+                          });
+  $('#apple1').mouseleave(function(){
+    $('#apple1').animate({padding: -'10px'
+                          });
+      });
+    });
+  });
